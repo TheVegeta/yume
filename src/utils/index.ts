474ab7f4f5e4f4cba14964696312b105cc7d19a2
TheVegeta <iamvegeta@protonmail.com>
2023-11-24 23:29:33 +0530

    cleanup the branch

--- src/utils/index.ts
+++
@@ -1,9 +0,0 @@
-export const voidFunction = () => {};
-
-export const handleArrayBuffer = (message: ArrayBuffer | string) => {
-  if (message instanceof ArrayBuffer) {
-    const decoder = new TextDecoder();
-    return decoder.decode(message);
-  }
-  return message;
-};
